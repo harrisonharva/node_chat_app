c64e5b398b1dedf99570fc1822fb64068749404d
harrisonharva <harrisonharva@gmail.com>
2017-07-27 11:48:37 +0530

    Create generateMessage utils

--- public/js/index.js
+++ public/js/index.js
@@ -1,32 +1,33 @@
 var socket = io();
 socket.on('connect', function() {
     console.log("Connected to Server");
-    // socket.emit('createMessage', {
-    //     from: "test1@example.com",
-    //     text: "This is testing test message",
-    // });
 });
 socket.on('disconnect', function() {
     console.log("Disconnected from server");
 });
 socket.on('newMessage', function(message){
-    console.log('New Message Received Event');
-    console.log(message);
     console.log("from :"+message.from);
     console.log("text :"+message.text);
     console.log("createdAt :"+message.createdAt);
+    console.log(message);
 });
 socket.on('newConnection', function(message){
-    console.log('New Connection Message Received Event');
-    console.log(message);
     console.log("from :"+message.from);
     console.log("text :"+message.text);
     console.log("createdAt :"+message.createdAt);
+    console.log(message);
 });
 socket.on('userDisconnected', function(message){
     console.log('User disconnected Event');
-    console.log(message);
     console.log("from :"+message.from);
     console.log("text :"+message.text);
     console.log("createdAt :"+message.createdAt);
+    console.log(message);
+});
+socket.emit('createMessage', {
+    from: "testUser1",
+    text: "Hi !!!!"
+}, function(data){
+    console.log('Got it');
+    console.log(data);
 });
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {generateMessage} = require('./utils/message');
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public' );
 var app = express();
+
 // Create http server  to integrate socketIO with ExpressJS
 var server = http.createServer(app);
 var io = socketIO(server);
@@ -15,6 +16,10 @@ var io = socketIO(server);
 // Set default path for templates and user interface files
 app.use(express.static(publicPath));
 
+/**
+ * [Event handeler when user connected to the server]
+ * @type {String}
+ */
 io.on('connection', (socket) => {
     console.log("New user connected");
 
@@ -24,15 +29,10 @@ io.on('connection', (socket) => {
     // socket.broadcast.emit from Admin to all the other user except new user to notify user joined
     socket.broadcast.emit('newConnection', generateMessage("Admin", "New user joined"));
 
-
-    // socket.emit('newMessage', {
-    //     from: 'test@example.com',
-    //     text: 'Test description for testing data',
-    //     createdAt: 123
-    // });
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message, callback) => {
         console.log("createMessage: ", message);
         io.emit('newMessage', generateMessage(message.from, message.text));
+        callback('This is from the server.');
         // socket.broadcast.emit('newMessage', {
         //     from: message.from,
         //     text: message.text,
@@ -42,8 +42,10 @@ io.on('connection', (socket) => {
 
     socket.on('disconnect', () => {
         console.log("User was disconnected");
+
         // socket.emit from Admin to disconnected user window in chat app
         socket.emit('userDisconnected', generateMessage("Admin", "Good bye!!!"));
+
         // socket.broadcast.emit from Admin to all the other user except new user to notify user disconnected
         socket.broadcast.emit('userDisconnected', generateMessage("Admin", "User disconnected from this conversation"));
     });
